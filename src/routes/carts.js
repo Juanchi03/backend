1e26ec748dd504dec62d8b8de739fceee7aec42b
Juanchi <juanchiviola@gmail.com>
2023-12-28 21:07:08 -0300

    hh

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -1,30 +1,97 @@
 const express = require('express');
-const cartManager = require('../managers/cartManager');
 const router = express.Router();
+const CartModel = require('../models/cartModel');
+const ProductModel = require('../models/productModel');
 
-router.post('/', (req, res) => {
-  const newCart = req.body;
-  const cart = cartManager.addCart(newCart);
-  res.json(cart);
-});
+// ...
 
-router.get('/:cid', (req, res) => {
-  const cartId = req.params.cid;
-  const cart = cartManager.getCartById(cartId);
-  res.json(cart ? cart.products : []);
-});
-
-router.post('/:cid/product/:pid', (req, res) => {
+router.delete('/:cid/products/:pid', async (req, res) => {
   const cartId = req.params.cid;
   const productId = req.params.pid;
-  const { quantity } = req.body;
-  const result = cartManager.addProductToCart(cartId, productId, quantity);
-  if (result) {
-    res.json(result);
-  } else {
-    res.status(404).json({ message: 'Carrito no encontrado' });
+
+  try {
+    const cart = await CartModel.findById(cartId);
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Carrito no encontrado' });
+    }
+
+    cart.products = cart.products.filter((product) => product.productId.toString() !== productId);
+
+    await cart.save();
+
+    res.status(200).json(cart);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar el producto del carrito', error: error.message });
   }
 });
 
-module.exports = router;
+router.put('/:cid', async (req, res) => {
+  const cartId = req.params.cid;
+  const updatedProducts = req.body.products;
+
+  try {
+    const cart = await CartModel.findById(cartId).populate('products.productId');
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Carrito no encontrado' });
+    }
+
+    cart.products = updatedProducts;
+
+    await cart.save();
+
+    res.status(200).json(cart);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al actualizar el carrito', error: error.message });
+  }
+});
+
+router.put('/:cid/products/:pid', async (req, res) => {
+  const cartId = req.params.cid;
+  const productId = req.params.pid;
+  const { quantity } = req.body;
+
+  try {
+    const cart = await CartModel.findById(cartId).populate('products.productId');
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Carrito no encontrado' });
+    }
+
+    const productIndex = cart.products.findIndex((p) => p.productId._id.toString() === productId);
+
+    if (productIndex !== -1) {
+      cart.products[productIndex].quantity = quantity;
+    }
+
+    await cart.save();
+
+    res.status(200).json(cart);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al actualizar la cantidad del producto en el carrito', error: error.message });
+  }
+});
+
+router.delete('/:cid', async (req, res) => {
+  const cartId = req.params.cid;
+
+  try {
+    const cart = await CartModel.findById(cartId);
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Carrito no encontrado' });
+    }
+
+    cart.products = [];
+
+    await cart.save();
+
+    res.status(200).json({ message: 'Productos eliminados del carrito exitosamente' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar los productos del carrito', error: error.message });
+  }
+});
+
+// ...
 
--- src/server.js
+++ src/server.js
@@ -1,109 +1,48 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const exphbs = require('express-handlebars');
-const mongoose = require('mongoose');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
 
-const PORT = 8080;
-
-mongoose.connect('mongodb+srv://<usuario>:<contraseña>@<cluster>.mongodb.net/ecommerce', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-
-const ProductDao = require('./src/dao/productDao');
-const CartModel = require('./src/models/cartModel'); 
-const CartDao = require('./src/dao/cartDao');
-const MessageDao = require('./src/dao/messageDao');
-
-app.get('/', async (req, res) => {
-  const products = await ProductDao.getAllProducts();
-  res.render('home', { products });
-});
-
-app.get('/realtimeproducts', async (req, res) => {
-  const products = await ProductDao.getAllProducts();
-  res.render('realTimeProducts', { products });
-});
-
-app.get('/chat', async (req, res) => {
-  const messages = await MessageDao.getAllMessages();
-  res.render('chat', { messages });
-});
-
-app.post('/api/products', async (req, res) => {
+app.get('/api/products', async (req, res) => {
   try {
-    const newProduct = req.body; 
-    const createdProduct = await ProductDao.addProduct(newProduct);
-    io.emit('newProduct', createdProduct);
-    res.status(201).json(createdProduct);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al agregar el producto' });
-  }
-});
+    const { limit = 10, page = 1, sort, query } = req.query;
 
-app.delete('/api/products/:productId', async (req, res) => {
-  const productId = req.params.productId;
-  try {
-    const deletedProduct = await ProductDao.deleteProduct(productId);
-    io.emit('deleteProduct', deletedProduct);
-    res.status(200).json(deletedProduct);
+    const skip = (page - 1) * limit;
+    const sortOption = sort === 'desc' ? -1 : 1;
+    
+    const filter = query ? { category: query } : {};
+
+    const products = await ProductModel.find(filter)
+      .sort({ price: sortOption })
+      .limit(parseInt(limit))
+      .skip(skip);
+
+    const totalProducts = await ProductModel.countDocuments(filter);
+    const totalPages = Math.ceil(totalProducts / limit);
+    const hasNextPage = page < totalPages;
+    const hasPrevPage = page > 1;
+    const nextPage = hasNextPage ? page + 1 : null;
+    const prevPage = hasPrevPage ? page - 1 : null;
+    const prevLink = hasPrevPage ? `/api/products?limit=${limit}&page=${prevPage}&sort=${sort}&query=${query}` : null;
+    const nextLink = hasNextPage ? `/api/products?limit=${limit}&page=${nextPage}&sort=${sort}&query=${query}` : null;
+
+    res.status(200).json({
+      status: 'success',
+      payload: products,
+      totalPages,
+      prevPage,
+      nextPage,
+      page,
+      hasPrevPage,
+      hasNextPage,
+      prevLink,
+      nextLink,
+    });
   } catch (error) {
-    res.status(500).json({ error: 'Error al eliminar el producto' });
+    res.status(500).json({ status: 'error', error: 'Error al obtener los productos' });
   }
 });
 
 
-app.get('/api/carts/:userId', async (req, res) => {
-  const userId = req.params.userId;
-  try {
-    const userCart = await CartDao.getCartByUserId(userId);
-    res.status(200).json(userCart);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al obtener el carrito del usuario' });
-  }
-});
-
-app.post('/api/carts/:userId/addProduct/:productId', async (req, res) => {
-  const userId = req.params.userId;
-  const productId = req.params.productId;
-  try {
-    const updatedCart = await CartDao.addProductToCart(userId, productId);
-    res.status(200).json(updatedCart);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al agregar el producto al carrito' });
-  }
-});
+app.use('/api/carts', require('./src/routes/carts'));
 
 
-
-io.on('connection', (socket) => {
-  console.log('Usuario conectado');
-
-  socket.on('sendMessage', async (message) => {
-    try {
-      await MessageDao.addMessage(message);
-      io.emit('message', message);
-    } catch (error) {
-      console.error('Error al guardar el mensaje:', error);
-    }
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Usuario desconectado');
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
 
 
